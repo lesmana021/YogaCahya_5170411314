7ea5c505ff6290714d7708392f33b52ab8370bc6
lesmana021 <44439147+lesmana021@users.noreply.github.com>
2018-10-24 21:24:25 +0700

    Add files via upload

---
+++ tugas/src/Program/YogaCahya_5170411314.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Program;
+import java.util.Scanner;
+
+public class YogaCahya_5170411314 {
+    public static void main(String[]args){
+
+int change;
+Scanner s = new Scanner (System.in);
+
+for (change=1; change<=3;change++)
+{
+    System.out.println("1. Input data ");
+    System.out.println("2. Tampil data ");
+    System.out.println("3. Keluar ");
+    System.out.print("Pilihan: "); change = s.nextInt();
+}
+        
+}  
+}
